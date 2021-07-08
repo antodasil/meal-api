6cbb7e2864a557772e48ea9534731957e5c9e640
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-07-08 18:35:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/hello', (_, res) => res.json('Hello world !'));
+
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
