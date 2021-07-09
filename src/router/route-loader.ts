d31175d552f3d0c498c497cd33b567bc25dd60e6
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-07-09 14:15:30 +0200

    Refacto route-loader

--- src/router/route-loader.ts
+++ src/router/route-loader.ts
@@ -14,6 +14,7 @@ class RouteLoaderError extends Error {
 
 export class RouteLoader {
 
+    /** Charge les routes depuis un fichier */
     async loadRoutes(app: Express, filePath?: string): Promise<boolean> {
 
         // Liste des routes
@@ -26,44 +27,20 @@ export class RouteLoader {
                 continue;
             }
 
-            // On supprime le '/' de fin s'il y en a un 
-            if(route.path.endsWith('/')) {
-                route.path = route.path.slice(0, route.path.length-1);
-            }
-
-            // On charge le controller
-            let module = await import(process.cwd() + '\\out\\controllers\\' + route.controller + '.controller.js');
-            let controller: BaseController = module?.controller;
+            // On récupère le controller
+            let controller: BaseController = await this.loadRouteController(route);
 
             // Création des routes pour ce controller
             if(!controller) {
                 console.log('Route ignored: Controller ' + route.controller + ' not found');
                 continue;
             }
-
-            if(controller.get) {
-                app.get(route.path + '/:id?', controller.get);
-            }
-            
-            if(controller.post) {
-                app.post(route.path, controller.post);
-            }
-            
-            if(controller.put) {
-                app.put(route.path + '/:id?', controller.put);
-            }
-            
-            if(controller.patch) {
-                app.patch(route.path + '/:id?', controller.patch);
-            }
-            
-            if(controller.delete) {
-                app.delete(route.path + '/:id?', controller.delete);
-            }
+            this.createRoutesFromController(app, route.path, controller);
         }
         return true;
     }
 
+    /** Obtient la liste des routes décritent dans le fichier donné */
     private getRoutesFromFile(filePath: string = '\\config\\routes.json'): Route[] {
         let routes: Route[];
 
@@ -82,5 +59,42 @@ export class RouteLoader {
         }
 
         return routes;
+    }
+
+    /** initialise le controller de la route */
+    private async loadRouteController(route: Route): Promise<any> {
+
+        // On charge le controller
+        let module = await import(process.cwd() + '\\out\\controllers\\' + route.controller + '.controller.js');
+        return module?.controller;
+    }
+
+    /** Créé les routes pour chaque méthode du controller */
+    private createRoutesFromController(app: Express, path: string, controller: BaseController): void {
+
+        // On supprime le '/' de fin s'il y en a un 
+        if(path.endsWith('/')) {
+            path = path.slice(0, path.length-1);
+        }
+
+        if(controller.get) {
+            app.get(path + '/:id?', controller.get);
+        }
+        
+        if(controller.post) {
+            app.post(path, controller.post);
+        }
+        
+        if(controller.put) {
+            app.put(path + '/:id?', controller.put);
+        }
+        
+        if(controller.patch) {
+            app.patch(path + '/:id?', controller.patch);
+        }
+        
+        if(controller.delete) {
+            app.delete(path + '/:id?', controller.delete);
+        }
     }
 }
