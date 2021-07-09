6d1b82d941bde8bd79ec862dcf225282be8561d5
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-07-09 10:22:53 +0200

    Update index.js to ES6

--- index.mjs
+++ index.mjs
@@ -1,9 +1,10 @@
-const express = require('express');
+import express from 'express';
+import { RouteLoader } from './out/router/RouteLoader.js';
+
 const app = express();
 const PORT = 3000;
 
-const routeLoader = require('./out/router/RouteLoader.js');
-(new routeLoader.RouteLoader()).loadRoutes(app).then(() => {
+(new RouteLoader()).loadRoutes(app).then(() => {
     app.listen(PORT, () => {
       console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
     });
