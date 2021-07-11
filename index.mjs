60a0d9825edd899daf26fb009300cb78a69911bd
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-07-11 18:04:00 +0200

    Add properties file support

--- index.mjs
+++ index.mjs
@@ -1,8 +1,15 @@
 import express from 'express';
 import { RouteLoader } from './out/router/route-loader.js';
+import dotenv from 'dotenv';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Load properties
+let propertiesLoadingRes = dotenv.config({path: 'config/properties.env', debug: true});
+if(propertiesLoadingRes.error) {
+    throw new Error("Failed to load properties");
+}
 
 // Load routes
 await(new RouteLoader()).loadRoutes(app);
