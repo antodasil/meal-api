94c6dcae489c1ab0e58ae11d999efce2911d17d5
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-07-12 10:17:29 +0200

    Add app-util.isDevelopmentEnv()

--- src/logger/init-logger.ts
+++ src/logger/init-logger.ts
@@ -1,5 +1,7 @@
 import winston from 'winston';
+import {isDevelopmentEnv} from 'app-utils';
 
+// List of log levels
 const levels = {
     error: 0,
     warn: 1,
@@ -8,34 +10,42 @@ const levels = {
     debug: 4
 }
 
+// Level max to print
 function level(): string {
-    return process.env.NODE_ENV === 'development' ? 'debug' : 'warn';
+    return isDevelopmentEnv() ? 'debug' : 'warn';
 }
 
-// Couleurs utilisés dans les logs
-// winston.addColors({
-//     error: 'red',
-//     warn: 'yellow',
-//     info: 'white',
-//     http: 'magenta',
-//     debug: 'green'
-// });
+// Colors used in logs
+if(isDevelopmentEnv()) {
+    winston.addColors({
+        error: 'red',
+        warn: 'yellow',
+        info: 'white',
+        http: 'magenta',
+        debug: 'green'
+    });
+}
 
+// Print format
 const format = winston.format.combine(
     winston.format.timestamp({format: 'DD/MM/YYYY HH:mm:ss:ms'}),
     // winston.format.colorize({all: true}),
     winston.format.printf(
         (info) => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`
     )
-)
+);
 
+// Log destination
+const transports = isDevelopmentEnv()
+    ? new winston.transports.Console()
+    : new winston.transports.File({filename: 'log/debug.log', handleExceptions: true});
+
+// Logger
 const Logger = winston.createLogger({
     level: level(),
     levels,
     format,
-    transports: process.env.NODE_ENV === 'development'
-        ? new winston.transports.Console()
-        : new winston.transports.File({filename: 'log/debug.log'})
+    transports
 });
 
 export default Logger;
