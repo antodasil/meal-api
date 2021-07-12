a3e9e20cf431c4d215c34975cd48a2d6b8cae279
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-07-12 10:18:55 +0200

    Fix port property

--- index.mjs
+++ index.mjs
@@ -6,14 +6,16 @@ import tsconfigPath from 'tsconfig-paths';
 // To use tsconfig.paths
 tsconfigPath.register();
 
-const app = express();
-const PORT = process.env.PORT || 3000;
-
 // Load properties
 let propertiesLoadingRes = dotenv.config({path: 'config/properties.env', debug: true});
 if(propertiesLoadingRes.error) {
     throw new Error("Failed to load properties");
 }
+
+// Create app
+const app = express();
+// Init port
+const PORT = process.env.PORT || 3000;
 
 // Load routes
 await(new RouteLoader()).loadRoutes(app);
