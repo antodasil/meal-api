f63e122aefd8853d64ddac5acbefaf1889e2cff6
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-07-12 10:51:45 +0200

    Update app-utils: Add comment

--- src/utils/app-utils.ts
+++ src/utils/app-utils.ts
@@ -1,3 +1,4 @@
-export function isDevelopmentEnv() {
+/** Return true if app started in development environment */
+export function isDevelopmentEnv(): boolean {
     return process.env.NODE_ENV === 'development';
 }
