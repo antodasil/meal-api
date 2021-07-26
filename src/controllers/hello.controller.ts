e87c333b6456bdcb9a96d3707e89262f1d31ca77
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-07-26 17:18:05 +0200

    HelloController Prettier

--- src/controllers/hello.controller.ts
+++ src/controllers/hello.controller.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from 'express-serve-static-core';
-import {BaseController} from 'base-controller';
+import { Request, Response } from "express-serve-static-core";
+import { BaseController } from "./base.controller";
 
 class HelloController extends BaseController {
-    get(req: Request, res: Response) {
-        res.json({'hello': 'world'});
-    }
+  get(req: Request, res: Response) {
+    res.json({ hello: "world" });
+  }
 }
 
 export const controller = new HelloController();
