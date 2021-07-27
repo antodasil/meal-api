c48f8cb39845debf97a44c64db91b4f303d972e7
Antoine DA SILVA <antoine.da-silva@maincare.fr>
2021-07-27 14:59:42 +0200

    Prettier logger

--- src/logger/init-logger.ts
+++ src/logger/init-logger.ts
@@ -1,51 +1,54 @@
-import winston from 'winston';
-import {isDevelopmentEnv} from 'app-utils';
+import winston from "winston";
+import { isDevelopmentEnv } from "../utils/app-utils";
 
 // List of log levels
 const levels = {
-    error: 0,
-    warn: 1,
-    info: 2,
-    http: 3,
-    debug: 4
-}
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
 
 // Level max to print
 function level(): string {
-    return isDevelopmentEnv() ? 'debug' : 'warn';
+  return isDevelopmentEnv() ? "debug" : "warn";
 }
 
 // Colors used in logs
-if(isDevelopmentEnv()) {
-    winston.addColors({
-        error: 'red',
-        warn: 'yellow',
-        info: 'white',
-        http: 'magenta',
-        debug: 'green'
-    });
+if (isDevelopmentEnv()) {
+  winston.addColors({
+    error: "red",
+    warn: "yellow",
+    info: "white",
+    http: "magenta",
+    debug: "green",
+  });
 }
 
 // Print format
 const format = winston.format.combine(
-    winston.format.timestamp({format: 'DD/MM/YYYY HH:mm:ss:ms'}),
-    // winston.format.colorize({all: true}),
-    winston.format.printf(
-        (info) => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`
-    )
+  winston.format.timestamp({ format: "DD/MM/YYYY HH:mm:ss:ms" }),
+  // winston.format.colorize({all: true}),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level.toUpperCase()}: ${info.message}`
+  )
 );
 
 // Log destination
 const transports = isDevelopmentEnv()
-    ? new winston.transports.Console()
-    : new winston.transports.File({filename: 'log/debug.log', handleExceptions: true});
+  ? new winston.transports.Console()
+  : new winston.transports.File({
+      filename: "log/debug.log",
+      handleExceptions: true,
+    });
 
 // Logger
 const Logger = winston.createLogger({
-    level: level(),
-    levels,
-    format,
-    transports
+  level: level(),
+  levels,
+  format,
+  transports,
 });
 
 export default Logger;
